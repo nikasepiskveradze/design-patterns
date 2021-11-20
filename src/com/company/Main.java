92a496c5233448feef0d5cc0a7cfe9b20879d7b6
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2021-11-21 02:54:56 +0400

    Implemented strategy pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import com.company.strategy.BlackAndWhiteFilter;
+import com.company.strategy.ImageStorage;
+import com.company.strategy.JpegCompressor;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    var imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
+    imageStorage.store("Test file name");
   }
 }
---
+++ src/com/company/strategy/HighContrastFilter.java
@@ -0,0 +1,8 @@
+package com.company.strategy;
+
+public class HighContrastFilter implements Filter {
+  @Override
+  public void apply(String fileName) {
+    System.out.println("Applying high contract filter");
+  }
+}
