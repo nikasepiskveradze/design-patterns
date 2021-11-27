7e13f100975beb1fc4686cf6b6752dd59822ed94
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2021-11-28 02:39:38 +0400

    Implemented Template method pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,11 @@
 package com.company;
 
-import com.company.strategy.BlackAndWhiteFilter;
-import com.company.strategy.ImageStorage;
-import com.company.strategy.JpegCompressor;
+import com.company.template.TransferMoneyTask;
 
 public class Main {
 
   public static void main(String[] args) {
-    var imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
-    imageStorage.store("Test file name");
+    var task = new TransferMoneyTask();
+    task.execute();
   }
 }
---
+++ src/com/company/template/AuditTrail.java
@@ -0,0 +1,7 @@
+package com.company.template;
+
+public class AuditTrail {
+  public void record() {
+    System.out.println("Audit");
+  }
+}
