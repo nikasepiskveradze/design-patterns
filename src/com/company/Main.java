9a4c1e137d5d761f25c4b3b5abfd58a0444386c2
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2021-11-30 23:11:04 +0400

    Implemented command pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import com.company.command.*;
+import com.company.command.fx.Button;
 import com.company.template.TransferMoneyTask;
 
 public class Main {
 
   public static void main(String[] args) {
-    var task = new TransferMoneyTask();
-    task.execute();
+    var composite = new CompositeCommand();
+    composite.add(new ResizeCommand());
+    composite.add(new BlackAndWhiteCommand());
+
+    composite.execute();
   }
 }
